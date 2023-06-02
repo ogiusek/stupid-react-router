150e9beed5fed90a12eea300831a078957767241
ogius <kowalewski.olgierd@gmail.com>
2023-06-03 01:48:21 +0200

    Updates

---
+++ precache-manifest.654418128a64fd81ac6eb868f267ed50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087c26968e05c1c6c4ffb6afeaa0ce31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c3cae833c73262c1889",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "ff5213d68a9bf20dad38",
+    "url": "./static/js/2.e5a3179b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e5a3179b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3cae833c73262c1889",
+    "url": "./static/js/main.c0c679d3.chunk.js"
+  },
+  {
+    "revision": "1ed93851847c48aa01d4",
+    "url": "./static/js/runtime-main.74639d10.js"
+  }
+]);
